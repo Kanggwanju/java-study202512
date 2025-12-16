92f06e8f4983ea629e9091e5e7b9e89398026be9
Kanggwanju <xprtktm1@gmail.com>
2025-12-16 22:51:34 +0900

    initial commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // 이 구문은 반복문을 사용합니다.
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
